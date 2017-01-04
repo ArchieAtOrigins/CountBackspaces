5964f37ee92a7e9bbdcf49e873ec7cb02029be0f
Archie Wells <archie@originscreative.com>
2017-01-04 08:51:06 -0800

    Minor updates to initial files

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // monitor user keystrokes
 // watch for the backspaces character
 // create  counter for backspaces.
+// Save current streak high score.
+
+
 var counterBs = 0;
 var counterNbs = 0;
 
@@ -8,10 +11,12 @@ window.addEventListener('keyup', (e) => {
 	const keyName = e.key;
 	if (e.key === 'Backspace') {
 		counterBs += 1;
+		document.getElementById('backspace').innerHTML = "You have use the backspace/delete key " + counterBs + " times";
 		console.log(counterBs);
 		console.log(keyName);
 	} else {
 		counterNbs += 1;
+		document.getElementById('noBackspace').innerHTML = "Your streak is " + counterNbs + " characters without hitting the backspace/delete key!";
 		console.log(" consecutive correct entries!")
 		if (e.key === 'Backspace') {
 			counterNbs = 0;
